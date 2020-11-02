152915679bad2044f74ed06b6ae91f9bc52f80fd
guo <904004593@qq.com>
2020-11-02 13:17:38 +0800

    first commit

---
+++ src/main/java/com/xst/XstApplication.java
@@ -0,0 +1,13 @@
+package com.xst;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XstApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XstApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xst/XstApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xst;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XstApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
